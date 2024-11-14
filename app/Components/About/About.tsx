b107b72a8afc30b75b2595f907f30705e3b6927a
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-11-14 16:58:08 -0500

    add about page

---
+++ app/Components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <div className="about w-full h-screen bg-emerald-400">
+    
+    </div>
+    </>
+  )
+}
+
+export default About
